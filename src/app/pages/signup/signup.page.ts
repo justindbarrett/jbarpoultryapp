4a78d75bea4dada64c5cfa956f2ea531c361fe03
Justin Barrett <justinbarrett25@gmail.com>
2024-04-29 13:59:21 -0600

    Add account creation code check

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -17,7 +17,9 @@ export class SignupPage implements OnInit {
   public name: string = "";
   public email: string = "";
   public password: string = "";
+  public creationCode: string = "";
   private disableSignUpButton = false;
+  private remainingAttempts = 5;
 
   constructor(
     private authService: AuthenticationService,
@@ -31,11 +33,24 @@ export class SignupPage implements OnInit {
   signUp() {
     this.disableSignUpButton = true;
     console.log('Sign Up');
-    this.authService.registerUser(this.email, this.password)
-    .then(auth => {
-         this.navCtrl.navigateForward("login");
-    })
-    .catch(err => { console.log(JSON.stringify(err)); this.presentAlert(err.code); })
+
+    // handle remaining attempts and disabling for some time use IP address/device ID?
+    if (this.remainingAttempts <= 0) {
+      return;
+    }
+
+    // TODO: store this code as a 10 digit secret
+    if (this.creationCode == "1234567890") {
+      this.authService.registerUser(this.email, this.password)
+      .then(auth => {
+           this.navCtrl.navigateForward("login");
+      })
+      .catch(err => { console.log(JSON.stringify(err)); this.presentAlert(err.code); })
+    }
+    else {
+      this.remainingAttempts = this.remainingAttempts - 1;
+      this.presentAlert(`Invalid account creation code. ${this.remainingAttempts} attempts remaining.`);
+    }
   }
 
   cancel() {
@@ -44,7 +59,7 @@ export class SignupPage implements OnInit {
   }
 
   disableSignUp(): boolean {
-    return !(this.name && this.email && this.password) || this.disableSignUpButton;
+    return !(this.name && this.email && this.password && this.creationCode) || this.disableSignUpButton;
   }
 
   async presentAlert(message: string) {
