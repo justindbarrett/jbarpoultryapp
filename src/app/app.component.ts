5eaaf082d6bb6a6ccaaf1df35dfb899cbdfc04a3
Justin Barrett <justinbarrett25@gmail.com>
2024-04-30 21:02:18 -0600

    Menu in working state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,77 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonSplitPane, IonButton, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { calendarOutline, calendarSharp, cubeOutline, cubeSharp, peopleOutline, peopleSharp } from 'ionicons/icons';
+import { Subscription } from 'rxjs';
+import { IdentityService } from './identity.service';
+import { AuthenticationService } from './authentication.service';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet],
+  imports: [RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonButton],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   public appPages = [
     { title: 'Customers', url: '/folder/customers', icon: 'people' },
     { title: 'Schedule', url: '/folder/schedule', icon: 'calendar' },
     { title: 'Lots', url: '/folder/lots', icon: 'cube' },
   ];
+  public userDisplayName = "";
+  private userDetailsSubscription = new Subscription();
+
   constructor(
-    private router: Router
+    private router: Router,
+    private identityService: IdentityService,
+    private authService: AuthenticationService,
+    private navCtrl: NavController,
+    private alertCtrl: AlertController
   ) {
     addIcons({ peopleOutline, peopleSharp, calendarOutline, calendarSharp, cubeOutline, cubeSharp });
   }
 
+  ngOnInit(): void {
+    this.userDetailsSubscription = this.identityService.getUserDetailsObservable().subscribe((userDetails) => {
+      this.userDisplayName = userDetails.displayName;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userDetailsSubscription.unsubscribe();
+  }
+
   hideMenu() {
-    return !(this.router.url === "/login");
+    return !(this.router.url == "/login" || this.router.url == "/forgotpassword" || this.router.url == "/signup");
+  }
+
+  gotoSettings() {
+    console.log("Go to settings");
+  }
+
+  logOut() {
+    console.log("logging out");
+    this.authService.logOutUser()
+      .then(auth => {
+        console.log(`User Auth: ${JSON.stringify(auth)}`);
+        const userDetails = { displayName: "", emailAddress: "" };
+        this.identityService.setUserDetails(userDetails);
+        this.navCtrl.navigateBack("login");
+      })
+      .catch(err => { console.log(JSON.stringify(err)); this.presentAlert(err.code); })
+  }
+
+  async presentAlert(message: string) {
+    const alert = await this.alertCtrl.create({
+      header: 'Failed Log Out Attempt',
+      message: message,
+      buttons: ['Try Again'],
+    });
+
+    await alert.present();
   }
  }
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonTitle, IonMenuButton, IonToolbar, IonButtons, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonHeader, IonButton } from '@ionic/angular/standalone';
-import { AppShellComponent } from '../app-shell/app-shell.component';
 import { CommonModule } from '@angular/common';
 import { addIcons } from 'ionicons';
 import { calendarOutline, calendarSharp, cubeOutline, cubeSharp, peopleOutline, peopleSharp, settingsOutline, settingsSharp } from 'ionicons/icons';
@@ -18,62 +17,16 @@ import { Subscription } from 'rxjs';
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive, IonHeader, IonMenuButton, IonToolbar, IonButtons, IonTitle, IonButton, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet]
 })
-export class FolderPage implements OnInit, OnDestroy {
+export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  public appPages = [
-    { title: 'Customers', url: '/folder/customers', icon: 'people' },
-    { title: 'Schedule', url: '/folder/schedule', icon: 'calendar' },
-    { title: 'Lots', url: '/folder/lots', icon: 'cube' },
-  ];
-  public userDisplayName = "";
-  public userEmail = "";
-  private userDetailsSubscription = new Subscription();
 
   constructor(
-    private navCtrl: NavController,
-    private alertCtrl: AlertController,
-    private identityService: IdentityService,
-    private authService: AuthenticationService
   ) {
     addIcons({ peopleOutline, peopleSharp, calendarOutline, calendarSharp, cubeOutline, cubeSharp, settingsOutline, settingsSharp });
   }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
-    this.userDetailsSubscription = this.identityService.getUserDetailsObservable().subscribe((userDetails) => {
-      this.userDisplayName = userDetails.displayName;
-      this.userEmail = userDetails.emailAddress;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.userDetailsSubscription.unsubscribe();
-  }
-
-  logOut() {
-    console.log("logging out");
-    this.authService.logOutUser()
-      .then(auth => {
-        console.log(`User Auth: ${JSON.stringify(auth)}`);
-        const userDetails = { displayName: "", emailAddress: "" };
-        this.identityService.setUserDetails(userDetails);
-        this.navCtrl.navigateBack("login");
-      })
-      .catch(err => { console.log(JSON.stringify(err)); this.presentAlert(err.code); })
-  }
-
-  gotoSettings() {
-    console.log("Go to settings");
-  }
-
-  async presentAlert(message: string) {
-    const alert = await this.alertCtrl.create({
-      header: 'Failed Log Out Attempt',
-      message: message,
-      buttons: ['Try Again'],
-    });
-
-    await alert.present();
   }
 }
